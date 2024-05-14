0469bff22fc256819ec89639da886615da6296c4
EnO33 <mattmar33@gmail.com>
2024-05-14 11:23:40 +0200

    Clerk authentication

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,8 +1,8 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
 
   return (
-    <p>
-      This is an authenticated route
-    </p>
+    <UserButton afterSignOutUrl="/" />
   );
 }
