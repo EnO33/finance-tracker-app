13baa136cad714ba8def7d0b0a850e176bb77311
EnO33 <mattmar33@gmail.com>
2024-05-15 11:33:27 +0200

    Update schema.ts

--- db/schema.ts
+++ db/schema.ts
@@ -1,11 +1,11 @@
+import { createInsertSchema } from "drizzle-zod";
 import { pgTable, text } from "drizzle-orm/pg-core";
-import {createInsertSchema } from "drizzle-zod";
 
 export const accounts = pgTable("accounts", {
-    id: text("id").primaryKey(),
-    plaidId: text("plaid_id"),
-    name: text("name").notNull(),
-    userId: text("user_id").notNull(),
+  id: text("id").primaryKey(),
+  plaidId: text("plaid_id"),
+  name: text("name").notNull(),
+  userId: text("user_id").notNull(),
 });
 
 export const insertAccountSchema = createInsertSchema(accounts);
