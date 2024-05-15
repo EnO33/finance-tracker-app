538592844cb13d431e02b1e9fbedea7a341b1cd7
EnO33 <mattmar33@gmail.com>
2024-05-15 11:32:15 +0200

    Update navigation.tsx

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -42,36 +42,30 @@ const Navigation = () => {
   const onClick = (href: string) => {
     router.push(href);
     setIsOpen(false);
-  }
+  };
 
   if (isMobile) {
     return (
-        <Sheet open={isOpen} onOpenChange={setIsOpen}>
-            <SheetTrigger>
-                <Button
-                    variant="outline"
-                    size="sm"
-                    className="font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition"
-                >
-                    <Menu className="size-4" />
-                </Button>
-            </SheetTrigger>
-            <SheetContent side="left" className="px-2">
-                <nav className="flex flex-col gap-y-2 pt-6">
-                    {routes.map((route) => (
-                        <Button
-                            variant={route.href === pathname ? "secondary" : "ghost"}
-                            key={route.href}
-                            onClick={() => onClick(route.href)}
-                            className="w-full justify-start"
-                        >
-                            {route.label}
-                        </Button>
-                    ))}
-                </nav>
-            </SheetContent>
-        </Sheet>
-    )
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger className="p-2 rounded-lg font-normal bg-white/10 hover:bg-white/20 hover:text-white border-none focus-visible:ring-offset-0 focus-visible:ring-transparent outline-none text-white focus:bg-white/30 transition">
+          <Menu className="size-4" />
+        </SheetTrigger>
+        <SheetContent side="left" className="px-2">
+          <nav className="flex flex-col gap-y-2 pt-6">
+            {routes.map((route) => (
+              <Button
+                variant={route.href === pathname ? "secondary" : "ghost"}
+                key={route.href}
+                onClick={() => onClick(route.href)}
+                className="w-full justify-start"
+              >
+                {route.label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    );
   }
 
   return (
