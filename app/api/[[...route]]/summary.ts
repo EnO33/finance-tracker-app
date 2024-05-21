519adae7477adccc80b1272007501b4af555c23d
EnO33 <mattmar33@gmail.com>
2024-05-21 13:23:41 +0200

    Updated fetchFinancialData function call with lastPeriodStart and lastPeriodEnd instead of startDate and endDate

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -80,8 +80,8 @@ const app = new Hono()
             );
             const [lastPeriod] = await fetchFinancialData(
                 auth.userId,
-                startDate,
-                endDate,
+                lastPeriodStart,
+                lastPeriodEnd,
             );
 
             const incomeChange = calculatePercentageChange(
