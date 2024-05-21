e7e0a643764d215bfec1af041b3ff282205a9540
EnO33 <mattmar33@gmail.com>
2024-05-21 11:14:58 +0200

    CSV Import transactions

--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { Loader2, Plus } from "lucide-react";
+import { useState } from "react";
 
 import { useNewTransaction } from "@/features/transactions/hooks/use-new-transaction";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/use-bulk-delete-transactions";
@@ -10,17 +11,43 @@ import { Button } from "@/components/ui/button";
 import { DataTable } from "@/components/data-table";
 import { Skeleton } from "@/components/ui/skeleton";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
 import { columns } from "./columns";
+import { UploadButton } from "./upload-button";
+import { ImportCard } from "./import-card";
+
+enum VARIANTS {
+  LIST = "LIST",
+  IMPORT = "IMPORT",
+}
+
+const INITIAL_IMPORT_RESULTS = {
+  data: [],
+  errors: [],
+  meta: {},
+};
 
 const TransactionsPage = () => {
+  const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+  const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+  const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+    setImportResults(results);
+    setVariant(VARIANTS.IMPORT);
+  };
+
+  const onCancelImport = () => {
+    setImportResults(INITIAL_IMPORT_RESULTS);
+    setVariant(VARIANTS.LIST);
+  }
+
   const newTransaction = useNewTransaction();
   const deleteTransactions = useBulkDeleteTransactions();
   const transactionsQuery = useGetTransactions();
   const transactions = transactionsQuery.data || [];
 
   const isDisabled =
-    transactionsQuery.isLoading ||
-    deleteTransactions.isPending
+    transactionsQuery.isLoading || deleteTransactions.isPending;
 
   if (transactionsQuery.isLoading) {
     return (
@@ -39,15 +66,32 @@ const TransactionsPage = () => {
     );
   }
 
+  if (variant === VARIANTS.IMPORT) {
+    return (
+      <>
+        <ImportCard 
+          data={importResults.data}
+          onCancel={onCancelImport}
+          onSubmit={() => {}}
+        />
+      </>
+    );
+  }
+
   return (
     <div className="max-w-screen-2xl mx-auto w-full pb-10 -mt-24">
       <Card className="border-none drop-shadow-sm">
         <CardHeader className="gap-y-2 lg:flex-row lg:items-center lg:justify-between">
-          <CardTitle className="text-xl line-clamp-1">Transactions History</CardTitle>
-          <Button onClick={newTransaction.onOpen} size="sm">
-            <Plus className="size-4 mr-2" />
-            Add new
-          </Button>
+          <CardTitle className="text-xl line-clamp-1">
+            Transactions History
+          </CardTitle>
+          <div className="flex flex-col lg:flex-row items-center gap-y-2 gap-x-2">
+            <Button className="w-full lg:w-auto" onClick={newTransaction.onOpen} size="sm">
+              <Plus className="size-4 mr-2" />
+              Add new
+            </Button>
+            <UploadButton onUpload={onUpload} />
+          </div>
         </CardHeader>
         <CardContent>
           <DataTable
---
+++ app/(dashboard)/transactions/upload-button.tsx
@@ -0,0 +1,29 @@
+import { UploadIcon } from "lucide-react";
+import { useCSVReader } from "react-papaparse";
+
+import { Button } from "@/components/ui/button";
+
+type Props = {
+    onUpload: (results: any) => void;
+};
+
+export const UploadButton = ({ onUpload }: Props) => {
+    const { CSVReader} = useCSVReader();
+
+    // TODO: Add a paywall
+
+    return (
+        <CSVReader onUploadAccepted={onUpload}>
+            {({ getRootProps }: any) => (
+                <Button 
+                    size="sm" 
+                    className="w-full lg:w-auto"
+                    {...getRootProps()}
+                >
+                    <UploadIcon className="size-4 mr-2" />
+                    Import
+                </Button>
+            )}
+        </CSVReader>
+    );
+};
